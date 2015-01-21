c4270b3990646cbb2c22959d226b0107655a1ae6
Xiaoxu <xiaoxul@andrew.cmu.edu>
2015-01-21 14:54:18 -0500

    ui initialization at engine controler

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/Main.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/Main.java
@@ -6,13 +6,7 @@ import edu.cmu.lti.bic.sbs.ui.UserInterfaceInitializationException;
 
 public class Main {
 	public static void main(String[] args) {
-		UserInterface ui = null;
-		try {
-			ui = new UserInterface(null);
-		} catch (UserInterfaceInitializationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		engineControler eng = new engineControler();
 		System.out.println("Finished Starting");
 	}
--- sbs/src/main/java/edu/cmu/lti/bic/sbs/engine/engineControler.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/engine/engineControler.java
@@ -1,5 +1,18 @@
 package edu.cmu.lti.bic.sbs.engine;
 
-public class engineControler {
+import edu.cmu.lti.bic.sbs.ui.UserInterface;
+import edu.cmu.lti.bic.sbs.ui.UserInterfaceInitializationException;
 
+public class engineControler {
+	public engineControler() {
+		UserInterface ui = null;
+		try {
+			// User interface initialization
+			System.out.println("Initializing the user interface");
+			ui = new UserInterface(null);
+		} catch (UserInterfaceInitializationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
