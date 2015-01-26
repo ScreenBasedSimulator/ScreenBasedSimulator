7c9b8c5f739ae5165865f2b8f37579faf14b124f
lonelyjohner <lonelyjohner@163.com>
2015-01-25 20:43:08 -0500

    commit without change

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/ui/MainWindow.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/ui/MainWindow.java
@@ -34,7 +34,7 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 900, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
