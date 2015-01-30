fc484634f38bc75e3da6b525951848de5b368c4a
CodeMySky <codemysky@hotmail.com>
2015-01-30 10:49:11 -0500

    add patient

---
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/simulator/Patient.java
@@ -0,0 +1,77 @@
+package edu.cmu.lti.bic.sbs.simulator;
+import edu.cmu.lti.bic.sbs.gson.*;
+
+enum Status{
+	great, good, not_good, bad, dying
+	
+}
+
+public class Patient {
+	
+	BloodPressure bp;
+	HeartRate hr;
+	OxygenLevel ol;
+	RepositoryRate rr;
+	
+	GraphicDisplay graDisplay;
+	
+	Status status;
+	
+	public Patient(BloodPressure bp, HeartRate hr, OxygenLevel ol,
+			RepositoryRate rr) {
+		super();
+		this.bp = bp;
+		this.hr = hr;
+		this.ol = ol;
+		this.rr = rr;
+	}
+
+	public BloodPressure getBp() {
+		return bp;
+	}
+
+	public void setBp(BloodPressure bp) {
+		this.bp = bp;
+	}
+
+	public HeartRate getHr() {
+		return hr;
+	}
+
+	public void setHr(HeartRate hr) {
+		this.hr = hr;
+	}
+
+	public OxygenLevel getOl() {
+		return ol;
+	}
+
+	public void setOl(OxygenLevel ol) {
+		this.ol = ol;
+	}
+
+	public RepositoryRate getRr() {
+		return rr;
+	}
+
+	public void setRr(RepositoryRate rr) {
+		this.rr = rr;
+<<<<<<< HEAD
+	}
+	
+
+	public void updateWithTool(Tool tool){
+		//set four parameters
+	}
+	
+	
+	public void updateWithDrugs(Drug drug, float dose){
+		//set four parameters
+	}
+	
+	
+	
+=======
+	}	
+>>>>>>> origin/ningnaw
+}
