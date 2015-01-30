ef74163a510d98dd4c0a9f23c13c674ad691795c
CodeMySky <codemysky@hotmail.com>
2015-01-30 12:27:57 -0500

    + prescription

---
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/gson/Prescription.java
@@ -0,0 +1,33 @@
+package edu.cmu.lti.bic.sbs.gson;
+
+public class Prescription {
+	private Drug drug;
+	private Double dose;
+	private String unit;
+	public Prescription (Drug drug, double dose, String unit) {
+		this.setDrug(drug);
+		this.setDose(dose);
+		this.setUnit(unit);
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public Prescription setUnit(String unit) {
+		this.unit = unit;
+		return this;
+	}
+	public Double getDose() {
+		return dose;
+	}
+	public Prescription setDose(Double dose) {
+		this.dose = dose;
+		return this;
+	}
+	public Drug getDrug() {
+		return drug;
+	}
+	public Prescription setDrug(Drug drug) {
+		this.drug = drug;
+		return this;
+	}
+}
