e793f665018928860498e8e7494b41881a12bc22
CodeMySky <codemysky@hotmail.com>
2015-02-05 10:32:47 -0500

    merge

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/Main.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/Main.java
@@ -1,8 +1,6 @@
 package edu.cmu.lti.bic.sbs;
 
 import edu.cmu.lti.bic.sbs.engine.Engine;
-import edu.cmu.lti.bic.sbs.ui.UserInterface;
-import edu.cmu.lti.bic.sbs.ui.UserInterfaceInitializationException;
 
 public class Main {
 	public static void main(String[] args) {
