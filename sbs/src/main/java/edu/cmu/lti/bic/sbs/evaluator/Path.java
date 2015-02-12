6f982b691615f16f196b2c2a59098163f0527f95
alfie <sxalfie@gmail.com>
2015-02-12 14:28:01 -0500

    modified Path

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/Path.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/Path.java
@@ -2,24 +2,28 @@ package edu.cmu.lti.bic.sbs.evaluator;
 
 import java.util.List;
 
+
+
 /**
  * 
- * @author victorzhao
+ * @author victorzhao, xings
  *
  */
-public class Path {
-	List<Step> pathIns;
-	
-	String tag;
+public class Path extends ArrayList<Step> {
+	String tag = ""; // tag the type of this path: goldStandard or actual
 
 	/**
 	 * The method that use to calculate the score between two paths.
 	 * @param p2
 	 * @return
 	 */
-	public double pathScore(Path p2) {
-		// Required this and p2
-		return 0.0;
+	
+	public void setTag(String tag){
+	  this.tag = tag;
+	}
+	
+	public String getTag(){
+	  return this.tag;
 	}
 	
 	public static void main(String[] args) {
