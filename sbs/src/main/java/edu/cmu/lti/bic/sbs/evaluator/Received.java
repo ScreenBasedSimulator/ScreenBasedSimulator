72d4d3c131403581912a091b1627a0cb1ffbe96d
rsun07 <rsun2014@gmail.com>
2015-02-23 00:05:11 -0500

    updata format and Java doc

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/Received.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/Received.java
@@ -14,32 +14,60 @@ public class Received {
 
   private Tool tool;
 
-  public void setPara(MedicalParameter medPara) {
-    this.medPara = medPara;
-  }
-
-  public void set(Drug drug, double dose) {
-    this.drug = drug;
-    this.dose = dose;
-  }
-
-  public void set(Tool eq) {
-    this.tool = eq;
-  }
-
-  public MedicalParameter getPara() {
+  /**
+   * @return the medPara
+   */
+  public MedicalParameter getMedPara() {
     return medPara;
   }
 
+  /**
+   * @param medPara the medPara to set
+   */
+  public void setMedPara(MedicalParameter medPara) {
+    this.medPara = medPara;
+  }
+
+  /**
+   * @return the drug
+   */
   public Drug getDrug() {
     return drug;
   }
 
+  /**
+   * @param drug the drug to set
+   */
+  public void setDrug(Drug drug) {
+    this.drug = drug;
+  }
+
+  /**
+   * @return the dose
+   */
   public double getDose() {
     return dose;
   }
 
+  /**
+   * @param dose the dose to set
+   */
+  public void setDose(double dose) {
+    this.dose = dose;
+  }
+
+  /**
+   * @return the tool
+   */
   public Tool getTool() {
     return tool;
   }
+
+  /**
+   * @param tool the tool to set
+   */
+  public void setTool(Tool tool) {
+    this.tool = tool;
+  }
+
 }
