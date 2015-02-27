68594edb5d0450244d1fe7366a55846ffb979e77
alfie <sxalfie@gmail.com>
2015-02-26 23:43:25 -0500

    updated setter and initializer

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/StepRule.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/StepRule.java
@@ -11,7 +11,7 @@ public class StepRule {
   double maxSimilarity = 1;
   double minScore = 0;
   double passSimilarityPenalty = 1;
-  double passScorePenaty = 0;
+  double passScorePenalty = 0;
   double timePenalty = 1;
   double dosePenalty = 1;
   
@@ -23,4 +23,44 @@ public class StepRule {
   public StepRule(String ruleFile, Step step){
     
   }
+  
+  public StepRule(double maxSc, double maxSi, double minSc, double passSiP, double passScP,
+          double timeP, double doseP){
+    maxScore = maxSc;
+    maxSimilarity = maxSi;
+    minScore = minSc;
+    passSimilarityPenalty = passSiP;
+    passScorePenalty = passScP;
+    timePenalty = timeP;
+    dosePenalty = doseP;
+  }
+  
+  public double maxScore(){
+    return maxScore;
+  }
+  
+  public double maxSimilarity(){
+    return maxSimilarity;
+  }
+  
+  public double minScore(){
+    return minScore;
+  }
+  
+  public double passSimilarityPenalty(){
+    return passSimilarityPenalty;
+  }
+  
+  public double passScorePenalty(){
+    return passScorePenalty;
+  }
+  
+  public double timePenalty(){
+    return timePenalty;
+  }
+  
+  public double dosePenalty(){
+    return dosePenalty;
+  }
+  
 }
