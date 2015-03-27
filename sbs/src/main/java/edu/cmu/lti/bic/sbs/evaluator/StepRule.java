f6f766141b35176933d7c4a591a569eef61af53d
rsun07 <rsun2014@gmail.com>
2015-03-26 22:16:26 -0400

    complete confliction

--- sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/StepRule.java
+++ sbs/src/main/java/edu/cmu/lti/bic/sbs/evaluator/StepRule.java
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-package edu.cmu.lti.bic.sbs.evaluator;
-
-import java.util.ArrayList;
-
-import edu.cmu.lti.bic.sbs.gson.Patient;
-import edu.cmu.lti.bic.sbs.gson.Prescription;
-import edu.cmu.lti.bic.sbs.gson.Tool;
-
-public class StepRule {
-  double maxScore = 1;
-  double maxSimilarity = 1;
-  double minScore = 0;
-  double passSimilarityPenalty = 1;
-  double passScorePenalty = 0;
-  double timePenalty = 1;
-  double dosePenalty = 1;
-  
-  
-  public StepRule(){
-    
-  }
-  
-  public StepRule(String ruleFile, Step step){
-    
-  }
-  
-  public StepRule(double maxSc, double maxSi, double minSc, double passSiP, double passScP,
-          double timeP, double doseP){
-    maxScore = maxSc;
-    maxSimilarity = maxSi;
-    minScore = minSc;
-    passSimilarityPenalty = passSiP;
-    passScorePenalty = passScP;
-    timePenalty = timeP;
-    dosePenalty = doseP;
-  }
-  
-  public double maxScore(){
-    return maxScore;
-  }
-  
-  public double maxSimilarity(){
-    return maxSimilarity;
-  }
-  
-  public double minScore(){
-    return minScore;
-  }
-  
-  public double passSimilarityPenalty(){
-    return passSimilarityPenalty;
-  }
-  
-  public double passScorePenalty(){
-    return passScorePenalty;
-  }
-  
-  public double timePenalty(){
-    return timePenalty;
-  }
-  
-  public double dosePenalty(){
-    return dosePenalty;
-  }
-  
-}
-||||||| merged common ancestors
-=======
 package edu.cmu.lti.bic.sbs.evaluator;
 
 import java.util.ArrayList;
@@ -147,4 +78,3 @@ public class StepRule {
   }
 
 }
->>>>>>> ryan_Evaluator
